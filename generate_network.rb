bd075a5fc9fd19af290851a4114060ffc916bd83
Joshua B. Bussdieker <jbussdieker@gmail.com>
2015-10-04 20:19:17 +0000

    Add network config

---
+++ generate_network.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'yaml'
+
+config = YAML.load(File.read("config.yaml"))
+
+config_raw = <<-END
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet static
+	address #{config["address"]}
+	netmask #{config["netmask"]}
+	gateway #{config["gateway"]}
+	dns-search #{config["domain"]}
+	dns-nameservers #{config["nameservers"].join(" ")}
+END
+
+puts config_raw
+
