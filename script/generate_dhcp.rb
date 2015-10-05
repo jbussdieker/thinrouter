41b88a3bc40cef2f4f59a5864a3d793d6d615504
Joshua B. Bussdieker <jbussdieker@gmail.com>
2015-10-05 01:26:56 +0000

    Fix search path after moving scripts

--- script/generate_dhcp.rb
+++ script/generate_dhcp.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-$: << File.expand_path("./../lib", __FILE__)
+$: << File.expand_path("./../../lib", __FILE__)
 require 'thinrouter'
 
 config = ThinRouter::DHCP.all.collect do |res|
